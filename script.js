5a63849d0d5089a01e2fcdbbea011f90eb01fd2a
Kyusai-Eruchi <kyusai.eruchi@gmail.com>
2026-07-15 02:07:20 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const sections=document.querySelectorAll(".content");
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+sections.forEach(section=>{
+
+observer.observe(section);
+
+});
