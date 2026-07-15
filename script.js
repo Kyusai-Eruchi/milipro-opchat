f0f9dc99592f784461c816c558a89967d5f401fd
Kyusai-Eruchi <kyusai.eruchi@gmail.com>
2026-07-15 14:44:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,31 @@ sections.forEach(section=>{
 observer.observe(section);
 
 });
+
+const menuToggle=document.getElementById("menu-toggle");
+
+const nav=document.getElementById("nav");
+
+const overlay=document.getElementById("overlay");
+
+menuToggle.addEventListener("click",()=>{
+
+menuToggle.classList.toggle("active");
+
+nav.classList.toggle("active");
+
+overlay.classList.toggle("active");
+
+});
+
+/* 背景クリックで閉じる */
+
+overlay.addEventListener("click",()=>{
+
+menuToggle.classList.remove("active");
+
+nav.classList.remove("active");
+
+overlay.classList.remove("active");
+
+});
